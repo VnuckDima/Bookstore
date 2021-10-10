29991bb374997ea944c65611cf3612bfefa17456
DimaVnuk <vnuchok311994@hotmail.com>
2021-10-11 02:03:10 +0300

    add

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const jsonServer = require('serv');
+const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
