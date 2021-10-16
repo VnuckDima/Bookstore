53c61f09bcddff74f34a2c5d41d6e0be43f9a6bf
DimaVnuk <vnuchok311994@hotmail.com>
2021-10-16 03:54:43 +0300

    add serv

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-const express = require('express');
- const favicon = require('express-favicon');
- const path = require('path');
- const port = process.env.PORT || 8080;
- 
- 
- const app = express();
- app.use(favicon(__dirname + '/build/favicon.png')); 
- 
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./public/db.json');
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
 
- app.use(express.static(__dirname));
- app.use(express.static(path.join(__dirname, 'build')));
- 
+const PORT = process.env.PORT || 3001;
 
- app.get('/ping', function (req, res) {
-  return res.send('pong');
- });
- 
+server.use(middlewares);
+server.use(router);
 
- app.get('/*', function (req, res) {
- res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
- app.listen(port);
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
